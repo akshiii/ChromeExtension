74af5607a8cdde61958d1164ca90206c8d9dc41e
Akshita Tyagi <aksitatyagi1001@gmail.com>
2024-12-28 18:44:55 +0530

    on toggle clipboard script

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-console.log("Thisboo");
+console.log("Starting chrome extension");
 // navigator.serviceWorker.addEventListener("message", (event) => {
 //   console.log(event.data.msg, event.data.url);
 // });
 
 window.addEventListener("load", (event) => {
   let enableClipboard = document.getElementById("enable_clipboard");
+  //Fetch from chrome's storage
+  chrome.storage.local.get(["clipboard_enabled"]).then((result) => {
+    console.log("Value is " + result.key);
+    if (result.key?.enable_clipboard) {
+      result.key.enable_clipboard
+        ? (enableClipboard.checked = true)
+        : (enableClipboard.checked = false);
+    }
+  });
   enableClipboard.addEventListener("change", onCheckboxToggle);
 });
 
 function onCheckboxToggle(event) {
   console.log("on checkbox change = ", event.target.checked);
+  //Store in chrome's storage
   chrome.storage.local.set({
     clipboard_enabled: event.target.checked,
-  });
-
-  chrome.storage.local.get(["clipboard_enabled"]).then((result) => {
-    console.log("Value is " + result.key.enable_clipboard);
   });
 
   navigator.serviceWorker.ready.then((registration) => {
